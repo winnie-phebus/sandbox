a0ca884c26f7caa095b319fd0360400f75494c76
windorland <winn.phebus@gmail.com>
2025-01-28 23:44:04 -0500

    backup earlier svg playwork

---
+++ svelteland/svelteland/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import RoundHeart from "./assets/base/svg/round_heart.svelte";
+
+  let width = $state(50);
+  const roundHeart = $derived({
+    color: "red",
+    cx: 100,
+    cy: 100,
+    width: width,
+    cr: 1,
+    cfill: "red",
+  });
+</script>
+
+<main>
+  <div class="card">
+    <RoundHeart {...roundHeart} />
+    <p>width: {width}</p>
+    <input type="range" min="1" max="100" bind:value={width} />
+  </div>
+</main>
+
+<style>
+</style>
---
+++ svelteland/svelteland/src/assets/base/svg/round_heart.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  let {
+    color = "black",
+    cx = 100,
+    cy = 100,
+    width = 50,
+    cr = 1,
+    cfill = "red",
+  } = $props();
+
+  //     color = $state(color);
+  //   cx = $state(100);
+  //   cy = $state(100);
+  //   let width = $state(50);
+  //   let cr = $state(1);
+  //   let cfill = $state("red");
+
+  function pathPoint(x: number, y: number) {
+    return `${x} ${y}`;
+  }
+
+  const halfRadius = $derived(width / 2);
+
+  const minX = $derived(cx - width);
+  const lcx = $derived(cx - halfRadius); // left center x-coordinate
+  const rcx = $derived(cx + halfRadius); // right center x-coordinate
+  const maxX = $derived(cx + width);
+
+  // <path d="M50 100 C50 175 150 175 150 100 Z" />
+  const curveY = $derived(cy + (cy - halfRadius));
+
+  const curveStart = $derived(pathPoint(minX, cy));
+  const curveMin = $derived(pathPoint(minX, curveY));
+  const curveMax = $derived(pathPoint(maxX, curveY));
+  const curveStop = $derived(pathPoint(maxX, cy));
+
+  const curvePath = $derived(
+    `M${curveStart} C${curveMin} ${curveMax} ${curveStop}`
+  );
+
+  function setColor(newColor: string) {
+    color = newColor;
+  }
+
+  function setWidth(newWidth: number) {
+    width = newWidth;
+  }
+</script>
+
+<svg
+  width="200"
+  height="200"
+  viewBox="0 0 200 200"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <g id="round_heart" fill={color}>
+    <circle cx={lcx} {cy} r={halfRadius} />
+    <circle cx={rcx} {cy} r={halfRadius} />
+    <path d={curvePath} />
+    <circle {cx} {cy} r={cr} stroke="black" stroke-width="1" fill={cfill} />
+  </g>
+</svg>
